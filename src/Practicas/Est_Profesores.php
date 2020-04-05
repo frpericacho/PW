0cd928d0423deee879c71942cb31d2fe8afc365c
frpericacho <frpericacho@gmail.com>
2020-04-05 16:49:40 +0200

    version 9

--- src/Practicas/Est_Profesores.php
+++ src/Practicas/Est_Profesores.php
@@ -4,8 +4,6 @@
 	<body>
 		<a href="EstPr_media.php">Media de Profesor por Pregunta</a>
 		<br>
-        <a href="EstPr_mejorcalif.php">Mejor pregunta calificada al profesor</a>
-		<br>
 
 	</body>
 </html>
